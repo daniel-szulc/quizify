77a1207f03266d875207380eb2b3f718c1c230ee
Daniel Szulc <daniel.szulc198@gmail.com>
2023-06-12 19:22:45 +0200

    bug fixed css styled app build

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  baseHref: '/#',
+  firebase: {
+    databaseURL: "YOUR_CLOUD_FIRESTORE_DATABASE_URL",
+    apiKey: "YOUR_APP_ID",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_ID",
+    appId: "YOUR_APP_ID",
+    measurementId: "YOUR_MEASUREMENT_ID"
+  }
+};
